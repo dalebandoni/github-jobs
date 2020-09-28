95e708a61c8f19f4504b09f31e854b594cb526f5
Dale Bandoni <dalebandoni.10@gmail.com>
2020-09-28 14:12:38 +0100

    Updates

---
+++ precache-manifest.602020db9f9dd51700ebf6a30f1d827f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86cf27cd1a15b9598fc8e069a7270d12",
+    "url": "/github-jobs/index.html"
+  },
+  {
+    "revision": "6a3642194638a0ccdf3c",
+    "url": "/github-jobs/static/js/2.ea2eba7e.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/github-jobs/static/js/2.ea2eba7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95d496780116ad52144a",
+    "url": "/github-jobs/static/js/main.0bb10c61.chunk.js"
+  },
+  {
+    "revision": "818ca7b73b62caac58fd",
+    "url": "/github-jobs/static/js/runtime-main.90d76a25.js"
+  }
+]);
